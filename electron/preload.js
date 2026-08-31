f71988906fcf1ddf7bd5947de3e256c7f71037c5
ARSH <arshs287@gmail.com>
2026-08-31 22:02:26 +0530

    Updated Console errors

--- electron/preload.js
+++ electron/preload.js
@@ -1,4 +1,4 @@
-import { contextBridge, ipcRenderer } from 'electron';
+const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('nativeAPI', {
   notify: (title, body) => ipcRenderer.invoke('native:notify', { title, body }),
